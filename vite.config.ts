1943e9251b8afdb5c6e6dda5f0a6a632a73813d4
fabioalexandre1911 <SEU-EMAIL-GITHUB@example.com>
2025-11-18 14:12:59 -0300

    Fix base path to /portfolio/

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", // 👈 o nome EXATO do repositório
+  base: "/portfolio/", // ← SEM "f" extra, exatamente como o repositório
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
